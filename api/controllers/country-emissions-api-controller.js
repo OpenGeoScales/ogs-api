64c3cc4ec3bcebfafa43e0ead0026736ad064069
jil monpcjil <g.bunouf@paris-turf.com>
2021-05-10 22:07:36 +0200

    Add filter

--- api/controllers/country-emissions-api-controller.js
+++ api/controllers/country-emissions-api-controller.js
@@ -1,10 +1,44 @@
 const CountryEmissionsApi = require('../models/country-emission-api');
 
 exports.getCountryEmissionByCountry = (req, res, next) => {
-    CountryEmissionsApi.find({'geoComponent.geoComponent_name': req.query.country}).then(
+    let condition = [];
+
+    for(let param in req.query){
+        if(param == 'country') continue;
+        if(param == 'date')
+        {
+            condition.push({ "$eq" : [ "$$emissions.date", parseInt(req.query.date) ] });
+        }
+        else
+        {
+            condition.push({ "$eq" : [ "$$emissions."+param, req.query[param] ] });
+        }
+    };
+    console.log(condition);
+
+    CountryEmissionsApi.aggregate([
+        {
+            "$match" : {
+                'geoComponent.geoComponent_name': req.query.country
+            }
+        },
+        {
+            "$project" : {
+                "geoComponent" : 1,
+                "emissions" : {
+                    "$filter" : {
+                        "input" : "$emissions",
+                        "as" : "emissions",
+                        "cond" : {
+                            "$and" : condition
+                        }
+                    }
+                }
+            }
+        }
+        ]).then(
         (country_emissions_api) => {
             console.log(country_emissions_api.length + " CountryEmissions for country " + req.query.country + " api found");
-
             res.status(200).json(country_emissions_api);
         }
     ).catch(
@@ -15,6 +49,28 @@ exports.getCountryEmissionByCountry = (req, res, next) => {
         }
     );
 };
+
+// exports.getCountryEmissionByCountry = (req, res, next) => {
+//     CountryEmissionsApi.find({
+//         'geoComponent.geoComponent_name': req.query.country//},
+//         //{'emissions' : { $elemMatch : { date : "1903" } }
+//         //'emissions.date': 1903//req.query.date
+//     }).then(
+//         (country_emissions_api) => {
+//             console.log(country_emissions_api.length + " CountryEmissions for country " + req.query.country + " api found");
+//             let emissions = country_emissions_api.emissions;
+//             let ret = country_emissions_api.emissions.filter(emission=>emission.date = 1903);
+//             res.status(200).json(ret);
+//             //res.status(200).json(country_emissions_api);
+//         }
+//     ).catch(
+//         (error) => {
+//             res.status(400).json({
+//                 error: error
+//             });
+//         }
+//     );
+// };
 
 exports.getAllCountryEmission = (req, res, next) => {
     CountryEmissionsApi.find().then(
